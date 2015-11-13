844f958a53be1c93644f1b25502a62ee9fcdca78
sumorocker <macdonald.allan@live.com>
2015-11-13 20:15:57 +0000

    Question #5

--- answers.js
+++ answers.js
@@ -32,7 +32,7 @@ console.log (returnCharAndNum("Monkey",3));
 console.log (returnCharAndNum("Crocodile", 4));
 console.log (returnCharAndNum(" ", 1));
 
-//two numbers and multiplied together
+//two numbers added together
 
 function addNum(number1, number2){
     return number1 + number2;
@@ -41,3 +41,14 @@ function addNum(number1, number2){
 console.log (addNum( 2 , 4));
 console.log (addNum(50 ,78));
 console.log(addNum("blue" , "red"));
+
+// when I added blue and red it added the words together into an amazing new color called bluered(purple)
+
+function multiplyNum(number1, number2){
+    return number1 * number2;
+}
+
+console.log (multiplyNum(6 , 6));
+console.log (multiplyNum(36, 36));
+console.log (multiplyNum ("jeans" , 5));
+console.log (multiplyNum("jeans" , "nose"));
