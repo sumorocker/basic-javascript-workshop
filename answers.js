517b903ba648bbfe5c36ccfe5be24ea225b577e9
sumorocker <macdonald.allan@live.com>
2015-11-13 19:16:55 +0000

    test1

--- answers.js
+++ answers.js
@@ -1,4 +1,5 @@
 // returning first character of a string
+
 function firstLetter(string) {
     return string.charAt(0);
 };
@@ -7,6 +8,6 @@ console.log(firstLetter("Canada"));
 
 console.log(firstLetter("Sweden"));
 
-console.log(firstLetter(""))
+console.log(firstLetter(""));
 
-
+console.log(firstLetter("Argentina"));
